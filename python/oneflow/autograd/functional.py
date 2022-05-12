1f492ee42753455742224f416a8ff55836090bf1
QiangX-man <445653859@qq.com>
2022-05-12 08:31:01 +0000

    rm some  Interface comments

--- python/oneflow/autograd/functional.py
+++ python/oneflow/autograd/functional.py
@@ -365,60 +365,6 @@ def hessian(func: Callable, inputs, create_graph: bool = False, strict: bool = F
     res = jacobian(jac_func, inputs, create_graph=create_graph, strict=strict, vectorize=vectorize)
     return _tuple_postprocess(res, (is_inputs_tuple, is_inputs_tuple))
 
-
-#def jvp(func: Callable, primals: Any, tangents: Any, *, strict: bool = False, has_aux: bool = False):
-    """
-    Standing for the Jacobian-vector product, returns a tuple containing
-    the output of `func(*primals)` and the "Jacobian of ``func`` evaluated at
-    ``primals``" times ``tangents``. This is also known as forward-mode autodiff.
-
-    Args:
-        func (function): A Python function that takes one or more arguments,
-            one of which must be a Tensor, and returns one or more Tensors
-        primals (Tensors): Positional arguments to :attr:`func` that must all be
-            Tensors. The returned function will also be computing the
-            derivative with respect to these arguments
-        tangents (Tensors): The "vector" for which Jacobian-vector-product is
-            computed. Must be the same structure and sizes as the inputs to
-            ``func``.
-        has_aux (bool): Flag indicating that :attr:`func` returns a
-            ``(output, aux)`` tuple where the first element is the output of
-            the function to be differentiated and the second element is
-            other auxiliary objects that will not be differentiated.
-            Default: False.
-
-    Returns:
-        Returns a ``(output, jvp_out)`` tuple containing the output of ``func``
-        evaluated at ``primals`` and the Jacobian-vector product.
-        If ``has_aux is True``, then instead returns a ``(output, jvp_out, aux)`` tuple.
-
-    .. warning::
-        PyTorch's forward-mode AD coverage on operators is not very good at the
-        moment. You may see this API error out with "forward-mode AD not
-        implemented for operator X". If so, please file us a bug report and we
-        will prioritize it.
-
-    jvp is useful when you wish to compute gradients of a function R^1 -> R^N
-
-        >>> from functorch import jvp
-        >>> x = torch.randn([])
-        >>> f = lambda x: x * torch.tensor([1., 2., 3])
-        >>> value, grad = jvp(f, (x,), (torch.tensor(1.),))
-        >>> assert torch.allclose(value, f(x))
-        >>> assert torch.allclose(grad, torch.tensor([1., 2, 3]))
-
-    :func:`jvp` can support functions with multiple inputs by passing in the
-    tangents for each of the inputs
-
-         >>> from functorch import jvp
-         >>> x = torch.randn(5)
-         >>> y = torch.randn(5)
-         >>> f = lambda x, y: (x * y)
-         >>> _, output = jvp(f, (x, y), (torch.ones(5), torch.ones(5)))
-         >>> assert torch.allclose(output, x + y)
-
-    """
-
     
     
     
